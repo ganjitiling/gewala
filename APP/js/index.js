bb7207d9302e21d5b30f2f1b459b3f884de7db5b
方友镜 <fang799336159@163.com>
2017-06-13 21:35:22 +0800

    提交文件

---
+++ APP/js/index.js
@@ -0,0 +1,80 @@
+/**
+ * Created by bin on 2017/5/16.
+ */
+var menuclick = true;
+$("#header-menu").click(function () {
+    if(menuclick){
+        $("#header-menu-pulldown").css("visibility", "visible");
+        menuclick = false;
+    }else{
+        $("#header-menu-pulldown").css("visibility", "hidden");
+        menuclick = true;
+    }
+});
+// 电影、影院标签-切换
+var n = 0;
+var width = window.screen.availWidth;
+var step = width/100;
+var leftpx = $("#tab-movie").css("margin-left");
+var left = parseInt(leftpx);
+$("#cinema-label").click(function () {
+        // console.info(left);
+        if(n < 100){
+            n++;
+            left -= step;
+            $("#tab-movie").css("margin-left",left+"px");
+            setTimeout(arguments.callee, 5);
+        }else {
+            $("#cinema-label").css({"color":"rgba(242,101,34,1)","border-bottom":"2px solid rgba(242,101,34,1)"});
+            $("#movie-label").css({"color":"rgba(41, 42, 42, 0.78)","border-bottom":"2px solid rgba(177,181,177,1)"});
+        }
+});
+$("#movie-label").click(function () {
+    // console.info(left);
+    if(n > 0){
+        n--;
+        left += step;
+        $("#tab-movie").css("margin-left",left+"px");
+        setTimeout(arguments.callee, 5);
+    }else {
+        $("#movie-label").css({"color":"rgba(242,101,34,1)","border-bottom":"2px solid rgba(242,101,34,1)"});
+        $("#cinema-label").css({"color":"rgba(41, 42, 42, 0.78)","border-bottom":"2px solid rgba(177,181,177,1)"});
+    }
+});
+
+// 电影详情-跳转
+$("#movie01").click(function () {
+    window.location.href = "movie.html";
+});
+$("#movie02").click(function () {
+    window.location.href = "movie.html";
+});
+$("#movie03").click(function () {
+    window.location.href = "movie.html";
+});
+$("#movie04").click(function () {
+    window.location.href = "movie.html";
+});
+$("#movie05").click(function () {
+    window.location.href = "movie.html";
+});
+$("#willsale01").click(function () {
+    window.location.href = "movie.html";
+});
+$("#willsale02").click(function () {
+    window.location.href = "movie.html";
+});
+$("#willsale03").click(function () {
+    window.location.href = "movie.html";
+});
+$("#willsale04").click(function () {
+    window.location.href = "movie.html";
+});
+$("#willsale05").click(function () {
+    window.location.href = "movie.html";
+});
+
+// 关闭底部提示栏
+$("#close-bar-bottom").click(function () {
+    $("#openAPP").css("display","none");
+});
